2e7e5701b345e0ec85eabaf7e2ad04d8ea9221e4
rannyeribaptist <rannyerirbatista@gmail.com>
2021-02-15 11:18:32 -0300

    deleted and undone last migration

--- db/migrate/20210214222006_create_table_company_unities_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateTableCompanyUnitiesUsers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :table_company_unities_users do |t|
-      t.bigint :company_unity_id
-      t.bigint :user_id
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_222006) do
+ActiveRecord::Schema.define(version: 2021_01_31_135559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,11 +142,6 @@ ActiveRecord::Schema.define(version: 2021_02_14_222006) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["cash_account_id"], name: "index_outgoings_on_cash_account_id"
-  end
-
-  create_table "table_company_unities_users", force: :cascade do |t|
-    t.bigint "company_unity_id"
-    t.bigint "user_id"
   end
 
   create_table "users", force: :cascade do |t|
