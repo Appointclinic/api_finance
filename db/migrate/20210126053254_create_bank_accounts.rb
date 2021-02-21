225f5c3253ee938687ddadb38cab66890046152f
rannyeribaptist <rannyerirbatista@gmail.com>
2021-02-21 15:43:05 -0300

    added institution_name to bank_accounts

--- db/migrate/20210126053254_create_bank_accounts.rb
+++ db/migrate/20210126053254_create_bank_accounts.rb
@@ -1,7 +1,8 @@
 class CreateBankAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :bank_accounts, id: :uuid do |t|
-      t.string :name
+      t.string :account_name
+      t.string :institution_name
       t.decimal :initial_value
       t.references :company_unity, type: :uuid, null: false, foreign_key: true
 
